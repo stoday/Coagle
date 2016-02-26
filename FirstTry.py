9d8a294fcef8b81f23c82d16f15ee12f97dc7256
stoday <stoday@gmail.com>
2016-02-27 07:25:23 +0800

    Version 0.2

--- FirstTry.py
+++ FirstTry.py
@@ -121,7 +121,6 @@ def index3():
 
 @app.route('/z4')
 def index4():
-    ts_raw = pd.DataFrame.from_csv('.\dataset\AWDAS2_201505_1_align.csv', infer_datetime_format=True)
     graphs = dict(
         data=[
             dict(
@@ -164,6 +163,42 @@ def index5():
                 'x0': 4,
                 'y0': 1,
                 'x1': 6,
+                'y1': 2,
+                'line': {'color': 'rgba(128, 0, 128, 1)', 'width': 2},
+                'fillcolor': 'rgba(128, 0, 128, 0.7)',
+            },
+        ]
+    }
+    graphs = {'data': data, 'layout': layout}
+
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index3.html', ids=['aaa'], graphJSON=graphJSON, xxx='xxx')
+
+
+@app.route('/z6')
+def index6():
+    ts_raw = pd.DataFrame.from_csv('.\dataset\AWDAS2_201505_1_align.csv', infer_datetime_format=True)
+    xx = ts_raw.index  # Can use the pandas data structures directly
+    yy = pd.Series(ts_raw['F2218'], index=ts_raw.index)
+    data = [go.Scatter(x=['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'], y=[1, 3, 6])]
+    layout = {'width': 600, 'height': 600,
+        'shapes': [
+            # unfilled Rectangle
+            {
+                'type': 'rect',
+                'x0': '2013-10-04 22:23:00',
+                'y0': 1.5,
+                'x1': '2013-10-14 22:23:00',
+                'y1': 3,
+                'line': {'color': 'rgba(128, 0, 128, 1)'},
+            },
+            # filled Rectangle
+            {
+                'type': 'rect',
+                'x0': '2013-10-24 22:23:00',
+                'y0': 1,
+                'x1': '2013-11-04 22:23:00',
                 'y1': 2,
                 'line': {'color': 'rgba(128, 0, 128, 1)', 'width': 2},
                 'fillcolor': 'rgba(128, 0, 128, 0.7)',
